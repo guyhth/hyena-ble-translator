cc9db5b932ece993a2d3d6bda88bf2122b78cf53
guyhth <guy.heath@me.com>
2026-09-17 21:07:36 +0100

    Add CSC wheel event state

--- hyena-garmin-bridge/GarminCSC.h
+++ hyena-garmin-bridge/GarminCSC.h
@@ -10,6 +10,8 @@ public:
 private:
   uint32_t _wheelRevolutions = 0;
   uint16_t _lastCrankRevolutions = 0;
+  uint16_t _wheelEventTime = 0;
   uint16_t _crankEventTime = 0;
+  uint32_t _lastWheelEventMs = 0;
   uint32_t _lastCrankEventMs = 0;
 };
