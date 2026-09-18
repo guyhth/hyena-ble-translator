75bb5f3a00e265fae278eb420a6785b91b938303
guyhth <guy.heath@me.com>
2026-09-18 09:41:54 +0100

    Track live CSC revolution state

--- hyena-garmin-bridge/GarminCSC.h
+++ hyena-garmin-bridge/GarminCSC.h
@@ -8,10 +8,15 @@ public:
   void update(float speedKph, float cadenceRpm);
 
 private:
+  static constexpr float WHEEL_CIRCUMFERENCE_M = 2.096f;
+
   uint32_t _wheelRevolutions = 0;
   uint16_t _lastCrankRevolutions = 0;
   uint16_t _wheelEventTime = 0;
   uint16_t _crankEventTime = 0;
-  uint32_t _lastWheelEventMs = 0;
-  uint32_t _lastCrankEventMs = 0;
+  uint32_t _lastUpdateMs = 0;
+  float _wheelRevolutionAccumulator = 0.0f;
+  float _crankRevolutionAccumulator = 0.0f;
+  bool _haveWheelEvent = false;
+  bool _haveCrankEvent = false;
 };
