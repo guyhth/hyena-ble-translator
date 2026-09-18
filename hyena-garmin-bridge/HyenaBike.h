3e9550e0fc86fd1ff9907bb9653631eb9b6121c8
guyhth <guy.heath@me.com>
2026-09-18 09:36:29 +0100

    Decode live Hyena speed and cadence

--- hyena-garmin-bridge/HyenaBike.h
+++ hyena-garmin-bridge/HyenaBike.h
@@ -4,7 +4,9 @@
 
 struct BikeTelemetry {
   float speedKph = 0.0f;
+  bool speedValid = false;
   float cadenceRpm = 0.0f;
+  bool cadenceValid = false;
   float powerWatts = 0.0f;
   bool powerValid = false;
   uint8_t batterySoc = 0;
